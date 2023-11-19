38aa3707932c03a362074da03b7cdec88a9d9bd1
Kenny <nkenny@santandertecnologia.com.ar>
2023-11-19 17:03:54 -0300

    feat: app and defaults

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import { RouterProvider } from 'react-router-dom'
+import { router } from './modules/common/components/Router/Router'
+import { UserContextProvider } from './modules/user/context/UserContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  )
+}
+
+export default App
