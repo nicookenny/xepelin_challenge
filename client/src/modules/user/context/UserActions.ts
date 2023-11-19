57a41ea97ebbeb7a6c64b7a4594d9ca8f36d6fd9
Kenny <nkenny@santandertecnologia.com.ar>
2023-11-19 18:55:58 -0300

    feat: update user context

--- client/src/modules/user/context/UserActions.ts
+++ client/src/modules/user/context/UserActions.ts
@@ -1,10 +1,14 @@
 /** @format */
 
+import { ITransaction } from '../../transaction/models'
 import { IUser } from '../model'
 
 export const ActionTypes = {
-  SET_CLIENT: 'SET_CLIENT',
-  CLEAR_CLIENT: 'CLEAR_CLIENT',
+  SET_USER: 'SET_USER',
+  CLEAR_USER: 'CLEAR_USER',
+  SET_ACCOUNT_ID: 'SET_ACCOUNT_ID',
+  ADD_TRANSACTION: 'ADD_TRANSACTION',
+  FILL_TRANSACTIONS: 'FILL_TRANSACTIONS',
 }
 
 export interface IUserAction {
@@ -13,10 +17,25 @@ export interface IUserAction {
 }
 
 export const setClient = (user: IUser) => ({
-  type: ActionTypes.SET_CLIENT,
+  type: ActionTypes.SET_USER,
   payload: user,
 })
 
 export const clearClient = () => ({
-  type: ActionTypes.CLEAR_CLIENT,
+  type: ActionTypes.CLEAR_USER,
+})
+
+export const setAccountId = (accountId: string) => ({
+  type: ActionTypes.SET_ACCOUNT_ID,
+  payload: accountId,
+})
+
+export const fillTransactions = (transactions: ITransaction[]) => ({
+  type: ActionTypes.FILL_TRANSACTIONS,
+  payload: transactions,
+})
+
+export const addTransaction = (transaction: ITransaction) => ({
+  type: ActionTypes.ADD_TRANSACTION,
+  payload: transaction,
 })
--- client/src/modules/user/context/UserReducer.ts
+++ client/src/modules/user/context/UserReducer.ts
@@ -1,19 +1,53 @@
 /** @format */
 
+import { ITransaction } from '../../transaction/models'
 import { ActionTypes, IUserAction } from './UserActions'
 import { IUserState } from './UserContext'
 
 export const UserReducer = (state: IUserState, action: IUserAction) => {
   switch (action.type) {
-    case ActionTypes.SET_CLIENT:
+    case ActionTypes.SET_USER:
       return {
         ...state,
-        client: action.payload,
+        user: action.payload,
       }
-    case ActionTypes.CLEAR_CLIENT:
+    case ActionTypes.CLEAR_USER:
       return {
         ...state,
-        client: null,
+        user: null,
+      }
+    case ActionTypes.SET_ACCOUNT_ID:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          accountId: action.payload,
+        },
+      }
+    case ActionTypes.FILL_TRANSACTIONS:
+      const sortedTransactions = action.payload.sort(
+        (a: ITransaction, b: ITransaction) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime()
+        }
+      )
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          transactions: sortedTransactions,
+        },
+      }
+    case ActionTypes.ADD_TRANSACTION:
+      const updated = [...state.user!.transactions, action.payload]
+      const sorted = updated.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime()
+      })
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          transactions: sorted,
+        },
       }
     default:
       return state
