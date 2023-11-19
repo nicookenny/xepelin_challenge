d8f40799fc6510373f1fc54e29ea0ac23283c07a
Kenny <nkenny@santandertecnologia.com.ar>
2023-11-19 13:27:43 -0300

    feat: server main files

---
+++ server/jest.config.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+}
